31eb21ae00ff4e0bc4e182fb24ed571c22f4edb7
khetkoong <parpofficial@gmail.com>
2022-02-11 02:28:18 +0700

    1 file modified

--- pages/profile.js
+++ pages/profile.js
@@ -37,11 +37,4 @@ const Profile = ({ user }) => {
   );
 };
 
-export async function getServerSideProps() {
-  await new Promise((resolve) => {
-    setTimeout(resolve, 500);
-  });
-  return { props: {} };
-}
-
 export default Profile;
